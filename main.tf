4c678ef116c70f479726eff6340166ab76bfdb44
Taylor Asplund <taylor.asplund@gmail.com>
2021-12-23 14:02:49 -0800

    Add load balancer module

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ module "vpc" {
   vpc_cidr     = var.vpc_cidr
   env          = var.env
 }
+
+module "loadbalancer" {
+  source       = "./modules/loadbalancer"
+  vpc          = module.vpc.vpc_id
+  subnets      = module.vpc.public_subnets
+}
